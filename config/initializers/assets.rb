322f35993cc4c59363424c955bd6cc77ce8eee5d
Alexander Logunov <unlovedru@gmail.com>
2015-09-17 00:06:22 +0300

    добавляем шрифты в прекомпайл

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,13 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('ember_app', 'public', 'assets')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += []
+# Rails.application.config.assets.precompile += ['font-awesome/fonts/fontawesome-webfont.eot']
+# Rails.application.config.assets.precompile << %r(font-awesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff|woff2?)$)
+Rails.application.config.assets.precompile << %r(fonts/open_sans/[\w-]+/[\w-]+\.(?:eot|svg|ttf|woff|woff2?)$)
+Rails.application.config.assets.precompile << %r(fonts/font-awesome/[\w-]+\.(?:eot|svg|ttf|woff|woff2?)$)
