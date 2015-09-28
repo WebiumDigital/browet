31c1ef831afbec0ff143484e4e43932946ada6fb
Alexander Logunov <unlovedru@gmail.com>
2015-09-28 16:28:13 +0300

    category new collections fix

--- ember_app/app/controllers/categories/new.js
+++ ember_app/app/controllers/categories/new.js
@@ -4,9 +4,11 @@ export default Ember.Controller.extend({
   pageTitle: 'Категории',
   actions: {
     save() {
+      this.get('model').set('collections', this.get('collections'));
       this.get('model').save().then(() => {
         this.transitionToRoute('categories.index');
       });
     }
-  }
+  },
+  collections: Ember.computed.map('model.collections', item => item)
 });
--- ember_app/app/routes/categories/new.js
+++ ember_app/app/routes/categories/new.js
@@ -3,5 +3,9 @@ import ProtectedApplicationRoute from 'browet/routes/protected_application';
 export default ProtectedApplicationRoute.extend({
   model() {
     return this.store.createRecord('category');
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    controller.set('availableCollections', this.store.findAll('collection'));
   }
 });
